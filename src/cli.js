cff945907aee9c13f917a28aa2a0f8b8e2402475
Сергей <Global252g@gmail.com>
2024-02-06 12:06:27 +0700

    asd

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,6 @@
-import 'readline-sync'
+import readlineSync from 'readline-sync';
 
 export default function user() {
-console.log(brain-games)
-console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
 }
