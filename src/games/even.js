0db4c3f562b40161834325a55a390fba7ff0f263
Сергей <Global252g@gmail.com>
2024-02-27 14:35:30 +0700

    asddgsujh

--- src/games/even.js
+++ src/games/even.js
@@ -18,6 +18,7 @@ const isEven = () => {
   }
   if (random % 2 === 0 && answer === 'no') {
     console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.Let's try again, ${name}!`);
+    counter += 10;
   }
   if (random % 2 === 1 && answer === 'no') {
     console.log('Correct!');
@@ -25,6 +26,7 @@ const isEven = () => {
   }
   if (random % 2 === 1 && answer === 'yes') {
     console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.Let's try again, ${name}!`);
+    counter += 10;
   }
 
   if (counter === 3) {
